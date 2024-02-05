d33f32c66503dbc09de33063ecc4bf003e76cbae
JoaquinCodesOften <joaquin.ifn@gmail.com>
2024-02-05 14:30:03 -0800

    Craft menu now opens the crafting book with the appropriate spells loaded.

--- Assets/Scripts/Overworld/Menus/OWCraftMenu.cs
+++ Assets/Scripts/Overworld/Menus/OWCraftMenu.cs
@@ -1,21 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OWCraftMenu : MonoBehaviour
 {
     private CombatPrefabRefer allies;
+    private PlayerUnit selectedPlayer;
     private Inventory ingredients;
+    private Button bookButton;
+    private EnchantMenu[] charBooks =  new EnchantMenu [3];
+    public Transform openBookParent, closedBookParent;
     
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+
+    void Start() {
+        allies = Resources.Load<CombatPrefabRefer>("SOs/Dynamic/CombatRefer");
+        booksSetup();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void booksSetup() {
+        foreach (Transform book in closedBookParent) {
+            int bookIndex = book.GetSiblingIndex();
+            bookButton = book.GetComponent<Button>();
+            bookButton.onClick.AddListener(() => onBookSelect(book.GetSiblingIndex()));
+            PlayerUnit selectedPlayer = allies.allyTeam[bookIndex].GetComponent<PlayerUnit>();
+            charBooks[bookIndex] = openBookParent.GetChild(bookIndex).GetComponent<EnchantMenu>();
+            bookButton.onClick.AddListener(() => charBooks[bookIndex].ListLoad(selectedPlayer));
+        }
+    }
+    
+    public void onBookSelect(int bookNum) {
+        openBookParent.GetChild(bookNum).gameObject.SetActive(true);
     }
 }
