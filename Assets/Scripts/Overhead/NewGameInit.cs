7b4047d1410cf1306849f59f282d7b3f929fbb4c
JoaquinCodesOften <joaquin.ifn@gmail.com>
2024-02-08 23:37:40 -0800

    Implemented the weapons reverting to default upon starting a new game.

--- Assets/Scripts/Overhead/NewGameInit.cs
+++ Assets/Scripts/Overhead/NewGameInit.cs
@@ -9,10 +9,23 @@ public class NewGameInit : MonoBehaviour
     [SerializeField] CombatPrefabRefer staticTeamState;
     [SerializeField] Inventory staticInvState;
 
+    [SerializeField] WeaponItem[] _defaults;
+    [SerializeField] WeaponItem[] _targets;
+
     public void onNewGame() {
         dynamicTeamState.allyTeam = staticTeamState.allyTeam;
         dynamicInvState.NPItemCounts = staticInvState.NPItemCounts;
         dynamicInvState.NPItemsIndex = staticInvState.NPItemsIndex;
         dynamicInvState.KeyItems = staticInvState.KeyItems;
+        weaponReset();
+    }
+
+    private void weaponReset() {
+        for (int i = 0; i < _defaults.Length; i++){
+            if (_defaults[i] != null) {
+                var jsonData = JsonUtility.ToJson(_defaults[i]);
+                JsonUtility.FromJsonOverwrite(jsonData, _targets[i]);
+            }
+        }
     }
 }
